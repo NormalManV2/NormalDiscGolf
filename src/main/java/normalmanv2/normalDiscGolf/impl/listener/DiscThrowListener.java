3190de9fc68e1cb147fc7fbb1322feae031ef6f3
NormalMan_V2 <106290271+NormalManV2@users.noreply.github.com>
2026-06-28 22:35:41 -0500

    Wait for disc landing before advancing turns

--- src/main/java/normalmanv2/normalDiscGolf/impl/listener/DiscThrowListener.java
+++ src/main/java/normalmanv2/normalDiscGolf/impl/listener/DiscThrowListener.java
@@ -2,33 +2,39 @@ package normalmanv2.normalDiscGolf.impl.listener;
 
 import normalmanv2.normalDiscGolf.impl.event.DiscThrowEvent;
 import normalmanv2.normalDiscGolf.impl.manager.task.TaskManager;
-import normalmanv2.normalDiscGolf.impl.round.FFARound;
 import normalmanv2.normalDiscGolf.impl.round.RoundImpl;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.UUID;
+import java.util.ArrayList;
 
 public class DiscThrowListener implements Listener {
 
     @EventHandler
     public void onThrow(DiscThrowEvent event) {
+        Player player = event.getPlayer();
+        if (player == null) {
+            event.setCancelled(true);
+            return;
+        }
 
         RoundImpl roundImpl = (RoundImpl) event.getRound();
 
         if (!event.isTurn()) {
             event.setCancelled(true);
-            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&4It is not your turn!"));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4It is not your turn!"));
+            return;
         }
 
-        for (BukkitTask foundTask : TaskManager.getActiveTasks().keySet()) {
-            if (TaskManager.getActiveTasks().get(foundTask).equals(event.getPlayer().getUniqueId())) {
+        for (BukkitTask foundTask : new ArrayList<>(TaskManager.getActiveTasks().keySet())) {
+            if (TaskManager.getActiveTasks().get(foundTask).equals(player.getUniqueId())) {
                 TaskManager.unregisterTask(foundTask);
             }
         }
+
         roundImpl.handleStroke(event.getThrowMechanic());
-        roundImpl.nextTurn();
     }
 }
