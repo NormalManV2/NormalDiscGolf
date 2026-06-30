ae56b867b34dbe2bac7a7e251a37e22af404176b
NormalMan_V2 <106290271+NormalManV2@users.noreply.github.com>
2026-06-29 22:53:44 -0500

    Fix driver launch vector reuse

--- src/main/java/normalmanv2/normalDiscGolf/impl/disc/Driver.java
+++ src/main/java/normalmanv2/normalDiscGolf/impl/disc/Driver.java
@@ -40,7 +40,7 @@ public class Driver extends DiscImpl {
 
         World world = player.getWorld();
         Vector direction = player.getEyeLocation().getDirection().normalize();
-        Location throwLoc = player.getEyeLocation().add(direction.multiply(2));
+        Location throwLoc = player.getEyeLocation().clone().add(direction.clone().multiply(2));
         ThrowMechanicImpl tm = (ThrowMechanicImpl) throwMechanic;
 
         ItemDisplay itemDisplay = world.spawn(throwLoc, ItemDisplay.class, display -> {
@@ -53,6 +53,7 @@ public class Driver extends DiscImpl {
             display.setBillboard(Display.Billboard.CENTER);
             display.setCustomName(ChatColor.translateAlternateColorCodes('&', this.getName()));
             display.setCustomNameVisible(true);
+            display.setPersistent(false);
         });
 
         int accuracyLevel = skills.getAccuracy().getLevel();
@@ -64,7 +65,7 @@ public class Driver extends DiscImpl {
         double baseVelocity = discBaseSpeed * (1 + (powerLevel * Constants.POWER_ADJUSTMENT));
         double finalVelocity = baseVelocity * (1 + (formLevel * Constants.FORM_ADJUSTMENT));
 
-        Vector velocity = direction.multiply(finalVelocity);
+        Vector velocity = direction.clone().multiply(finalVelocity);
 
         double maxSpread = Math.max(0.0, 0.05 - (accuracyLevel * Constants.ACCURACY_ADJUSTMENT));
         velocity.setX(velocity.getX() + (Math.random() * maxSpread - maxSpread / 2));
